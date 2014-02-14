14a7f3744ec3c82f6272df6ca045fa109b8f656a
Jeremy Gooch <goochjs@yahoo.co.uk>
2014-02-14 18:05:54 +0000

    Tests now incorporated

---
+++ src/test/java/org/goochjs/jskills/testHelpers/EloAssert.java
@@ -0,0 +1,46 @@
+package org.goochjs.jskills.testHelpers;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.goochjs.jskills.GameInfo;
+import org.goochjs.jskills.IPlayer;
+import org.goochjs.jskills.ITeam;
+import org.goochjs.jskills.PairwiseComparison;
+import org.goochjs.jskills.Player;
+import org.goochjs.jskills.Rating;
+import org.goochjs.jskills.Team;
+import org.goochjs.jskills.elo.EloRating;
+import org.goochjs.jskills.elo.TwoPlayerEloCalculator;
+
+public class EloAssert {
+    private static final double ErrorTolerance = 0.1;
+
+    public static void assertChessRating(TwoPlayerEloCalculator calculator,
+                                         double player1BeforeRating,
+                                         double player2BeforeRating,
+                                         PairwiseComparison player1Result,
+                                         double player1AfterRating,
+                                         double player2AfterRating)
+    {
+        Player<Integer> player1 = new Player<Integer>(1);
+        Player<Integer> player2 = new Player<Integer>(2);
+
+        Collection<ITeam> teams = Team.concat(
+            new Team(player1, new EloRating(player1BeforeRating)),
+            new Team(player2, new EloRating(player2BeforeRating)));
+
+        GameInfo chessGameInfo = new GameInfo(1200, 0, 200, 0, 0);
+
+        Map<IPlayer, Rating> result = calculator.calculateNewRatings(chessGameInfo, teams,
+            player1Result.equals(PairwiseComparison.WIN) ? new int[] { 1, 2 } :
+            player1Result.equals(PairwiseComparison.LOSE) ? new int[] { 2, 1 } :
+            /* Draw */ new int[] { 1, 1 });
+
+
+        assertEquals(player1AfterRating, result.get(player1).getMean(), ErrorTolerance);
+        assertEquals(player2AfterRating, result.get(player2).getMean(), ErrorTolerance);
+    }
+}
